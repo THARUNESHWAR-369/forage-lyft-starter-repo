a37e7a34e348e4efd59e83fdedb5a0eb9ac06d4c
THARUNESHWAR S <s.tharuneshwar.369@gmail.com>
2024-06-07 10:21:28 +0000

    unit test added

--- car.py
+++ car.py
@@ -1,18 +1,10 @@
-from abc import ABC, abstractmethod
+from serviceable import Serviceable
 
-from engine.engine import Engine
-from battery.battery import Battery
 
-from test.test_car import unittest
+class Car(Serviceable):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
 
-class Car(ABC):
-    def __init__(self, engine: Engine, battery: Battery) -> None:
-        self.engine= engine
-        self.battery=battery
-
-    @abstractmethod
-    def needs_service(self) -> bool:
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
+    def needs_service(self):
+        return self.engine.needs_service() or self.battery.needs_service()
