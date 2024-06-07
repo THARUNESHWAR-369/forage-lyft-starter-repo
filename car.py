8670309e501163d2824a87c89ceb7f78326ac1bf
THARUNESHWAR S <s.tharuneshwar.369@gmail.com>
2024-06-07 10:11:19 +0000

    Refactor code

--- car.py
+++ car.py
@@ -1,10 +1,18 @@
 from abc import ABC, abstractmethod
 
+from engine.engine import Engine
+from battery.battery import Battery
+
+from test.test_car import unittest
 
 class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+    def __init__(self, engine: Engine, battery: Battery) -> None:
+        self.engine= engine
+        self.battery=battery
 
     @abstractmethod
-    def needs_service(self):
+    def needs_service(self) -> bool:
         pass
+
+if __name__ == '__main__':
+    unittest.main()
